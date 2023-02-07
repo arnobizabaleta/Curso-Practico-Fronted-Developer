fb7b7fade5085a4561cc3e66f8bfcb6a72e7fcb3
mister rueda <ledyperez34@gmail.com>
2023-02-07 15:53:46 -0500

    aaaa

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktop_menu = document.querySelector(".desktop-menu");
+const burgerMenu = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggledesktopMenu);
 
 function toggledesktopMenu () {
   desktop_menu.classList.toggle("inactive");
+}
+
+burgerMenu.addEventListener("click", toggleburgerMenu);
+
+function toggleburgerMenu () {
+  mobileMenu.classList.toggle("inactive");
 }
